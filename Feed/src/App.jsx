ed639ac5faa9b3b939822ba939a465aa9ba67a8c
victor cauã <victorcaua03@gmail.com>
2025-01-21 04:24:10 -0300

    Estudo do uso de Propriedades

--- Feed/src/App.jsx
+++ Feed/src/App.jsx
@@ -8,6 +8,10 @@ export function App() {
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium error blanditiis cum itaque quasi temporibus dolorem, illum quo! Est corrupti et commodi fuga libero, molestias fugiat temporibus. Labore, quasi doloribus!"
       />
       
+      <Post 
+        author= "Pedro"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium error blanditiis cum itaque quasi temporibus dolorem, illum quo! Est corrupti et commodi fuga libero, molestias fugiat temporibus. Labore, quasi doloribus!"
+      />
     </div>
   )
-}
+} 
