9465ad00935f09e8d097a2c5590afb4a09cceb28
victor cauã <victorcaua03@gmail.com>
2025-01-23 04:08:35 -0300

    ajustes do conteudo do feed

--- Feed/src/components/Post.jsx
+++ Feed/src/components/Post.jsx
@@ -2,7 +2,7 @@ import styles from './Post.module.css';
 
 export function Post(){
     return (
-        <article className={styles.Post}>
+        <article className={styles.post}>
             <header>
                 <div className={styles.author}>
                     <img src="https:github.com/odrerir.png" />
@@ -12,9 +12,18 @@ export function Post(){
                     </div>
                 </div>
 
-                <time datetime="2022-05-11  00:15:30"></time>
+                <time datetime="2022-05-11  00:15:30"> Publicado a 1H</time>
             </header>
 
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p>👉 <a href="">jane.design/doctorcare</a></p>
+
+                <p> <a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
         </article>
     )
 }
