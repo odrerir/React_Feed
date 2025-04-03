20b5052c0bf3a8fcf63c61c767e0da2522fdb56a
victor cauã <victorcaua03@gmail.com>
2025-04-03 12:31:14 -0300

    Refatorando

--- Feed/src/App.jsx
+++ Feed/src/App.jsx
@@ -1,33 +1,64 @@
 import { Post } from "./components/Post";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+import styles from './App.module.css';
 
 import './Global.css';
 
-import styles from './App.module.css';
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https:github.com/maykbrito.png",
+      name: "Devon Fernandes",
+      role: "Educator @Rockectseat"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉  jane.design/doctorcare'}
+    ],
+    publishedAt: new Date("2022-05-15  15:00:00"),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https:github.com/odrerir.png",
+      name: "Victor Caua",
+      role: "Web Devoleper"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉 jane.design/doctorcare'}
+    ],
+    publishedAt: new Date("2022-05-11  15:00:00"),
+  }
+];
+
 
 export function App() {
   return (
     <div>
       <Header />
-      <div className={styles.Wrapper}> 
+      <div className={styles.Wrapper}>
         <Sidebar>
           sidebar
         </Sidebar>
 
         <main>
-          <Post 
-            author= "Diego"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium error blanditiis cum itaque quasi temporibus dolorem, illum quo! Est corrupti et commodi fuga libero, molestias fugiat temporibus. Labore, quasi doloribus!"
-          />
-          
-          <Post 
-            author= "Pedro"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium error blanditiis cum itaque quasi temporibus dolorem, illum quo! Est corrupti et commodi fuga libero, molestias fugiat temporibus. Labore, quasi doloribus!"
-          />
-
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
   )
-} 
+}
--- Feed/src/components/Post.jsx
+++ Feed/src/components/Post.jsx
@@ -3,35 +3,22 @@ import styles from './Post.module.css';
 import { Comment } from "./Comment";
 import { Avatar } from './Avatar';
 
-export function Post(){
+export function Post({author, publishedAt, content}) {
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https:github.com/maykbrito.png" />
+                    <Avatar src={author.avatarUrl}/>
                     <div className={styles.authorInfo}>
-                        <strong>Devon Fernandes</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time datetime="2022-05-11  00:15:30"> Publicado a 1h</time>
+                <time title='11 de maio as 15:00' dateTime='2022-05-11 08:15:00'>
+                    {publishedAt}
+                </time>
             </header>
-
-            <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-                <p>👉 {' '} <a href="">jane.design/doctorcare</a></p>
-
-                <p>
-                    <a href="">#novoprojeto</a>{' '}
-                    <a href="">#nlw</a>{' '}
-                    <a href="">#rocketseat</a>{' '}
-                </p>
-            </div>
-
             <form className={styles.commentForm}>
                 <strong>Deixe seu FeedBack</strong>
 
